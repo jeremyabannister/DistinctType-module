28a352c126b19af7f3e730cef6960edba5516960
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-05-01 01:14:12 +0200

    Minimized dependencies.

--- Package.swift
+++ Package.swift
@@ -16,15 +16,22 @@ let package = Package(
         
         ///
         .package(
-            url: "https://github.com/jeremyabannister/FoundationToolkit",
-            "0.6.0" ..< "0.7.0"
+            url: "https://github.com/jeremyabannister/ExpressionErgonomics",
+            "0.3.0" ..< "0.4.0"
+        ),
+        
+        ///
+        .package(
+            url: "https://github.com/jeremyabannister/RandomlyGeneratable-module",
+            "0.1.0" ..< "0.2.0"
         ),
     ],
     targets: [
         .target(
             name: "DistinctType-module",
             dependencies: [
-                "FoundationToolkit",
+                "ExpressionErgonomics",
+                "RandomlyGeneratable-module",
             ]
         ),
         .testTarget(
