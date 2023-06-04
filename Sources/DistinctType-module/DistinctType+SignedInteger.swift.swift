3321a682a985a18ec55ac578273ff232b52dbf7c
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-06-04 23:00:47 +0200

    Added `DistinctType+SignedInteger.swift`.

---
+++ Sources/DistinctType-module/DistinctType+SignedInteger.swift.swift
@@ -0,0 +1,10 @@
+//
+//  DistinctType+SignedInteger.swift
+//  
+//
+//  Created by Jeremy Bannister on 6/4/23.
+//
+
+///
+extension DistinctType: SignedInteger
+    where UnderlyingType: SignedInteger { }
