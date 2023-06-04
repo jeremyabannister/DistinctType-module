f09e42a14e2e0e42b581b9ea0e56f10545437fc6
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-06-04 22:53:55 +0200

    Added `DistinctType+ExpressibleByIntegerLiteral.swift`.

---
+++ Sources/DistinctType-module/DistinctType+ExpressibleByIntegerLiteral.swift
@@ -0,0 +1,19 @@
+//
+//  DistinctType+ExpressibleByIntegerLiteral.swift
+//  
+//
+//  Created by Jeremy Bannister on 6/4/23.
+//
+
+///
+extension DistinctType: ExpressibleByIntegerLiteral
+    where UnderlyingType: ExpressibleByIntegerLiteral {
+    
+    ///
+    public typealias IntegerLiteralType = UnderlyingType.IntegerLiteralType
+    
+    ///
+    public init (integerLiteral value: IntegerLiteralType) {
+        self.init(.init(integerLiteral: value))
+    }
+}
